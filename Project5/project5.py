b4e1065edca7f5b5ac196c773eb334f2f831d5a2
Scotrraaj Gopal - GHD <scotgopal@gmail.com>
2020-08-17 04:25:36 +0800

    Completed Project 5

---
+++ Project5/project5.py
@@ -0,0 +1,104 @@
+import string
+import random
+import os
+import time
+# import pandas as pd
+
+file = open('Project5/sowpods.txt')
+wordsWithNewLine = file.readlines()
+file.close()
+
+cleanWords = []
+
+for words in wordsWithNewLine:
+    if words.endswith('\n'):
+        replacedWord = words.replace('\n','')
+        cleanWords.append(replacedWord)
+
+def list2Str(inputList):
+    outputString = ''
+    for index in inputList:
+        outputString+=index
+    return outputString
+
+def playAgain():
+    try:
+        choice = int(input("Press 1 - Play again?\n\tOR\nNot 1 - Exit programme\t:"))
+        if choice==1:
+            pass
+        else:
+            print("Programme exiting...\n")
+            time.sleep(2)
+            global replay 
+            replay = False
+    except ValueError:
+        print("Invalid choice, try again...\n")
+        playAgain()
+
+def guessProcess():
+    try:
+        print('\n~~~Guessing Time~~~')
+        print("Correct Letters\t:", rightLetters)
+        print('Wrong Letters\t:', wrongLetters,'\n')
+        if mistakeCount <6:
+            trial = (input("What is your guess:")).lower()
+            if trial not in string.ascii_letters:
+                print("\nThis version of Hangman no numbers, smartie!")
+                raise ValueError
+            letterCheck(trial)
+        else:
+            print('You LOST!\n')
+            print('The correct word was:\n>>>>',selectedWord,'<<<<\n')
+            playAgain()
+    except ValueError:
+        print ('Invalid input! Input an alphabet.\n')
+        guessProcess()
+
+def letterCheck(trial):
+    if trial in selectedWordList:
+        rightLetters.append(trial)
+        guessIndex = -1
+        for element in selectedWordList:
+            guessIndex+=1
+            if element == trial:
+                global guessed #Call global variable guessed into the function
+                guessedList = list(guessed)
+                guessedList[guessIndex] = trial
+                guessed = list2Str(guessedList)       
+        if guessed == selectedWord:
+            print(len(selectedWord),"-letter word: " + guessed)
+            print("CONGRATULATIONS! YOU WIN!")
+            playAgain()
+
+        else:
+            print("\nAmazing!\n")
+            print(len(selectedWord),"-letter word: " + guessed)
+            print("Guess again!\n")
+            guessProcess()
+    else:
+        wrongLetters.append(trial)
+        global mistakeCount
+        mistakeCount+=1
+        print("Ooopsie... Wrong guess. You have", 6-mistakeCount,"tries left!")
+        guessProcess()
+
+
+replay = True
+while(replay):
+    os.system("cls")
+    print("------------------------")
+    print("--------HANGMAN---------")
+    print("------------------------\n")
+    print("Let's play a fun game of Hangman! You get 6 chances to guess the whole word!\n")
+    # random.seed(3)
+    selectedWord = random.choice(cleanWords)
+    # print("Selected word: ", selectedWord)
+    guessed = "-"*len(selectedWord)
+    print('Challenge: \nGuess this', len(selectedWord),"-letter word: " + guessed)
+    selectedWordList = list(selectedWord)
+    
+    mistakeCount = 0
+    rightLetters = []
+    wrongLetters = []
+    
+    guessProcess() #jump to guess function
